9eabe8ac440d3b54be636e88ba59970371b43705
ciandev <ip4c0@icloud.com>
2021-02-01 21:40:35 -0600

    Quote Generator v1.0

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-console.log('Test');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+const errorLimit = 5;
+let errorCount = 0;
+
+// Show Loading Spinner
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading Spinner
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+//Get quote from API
+async function getQuote() {
+    showLoadingSpinner();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unkonw'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unkonw';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        // Stop Loader
+        removeLoadingSpinner();
+
+    } catch (error) {
+        if (errorCount < errorLimit) {
+            errorCount++;
+            getQuote();
+        } else {
+            removeLoadingSpinner();
+            twitterBtn.hidden = true;
+            newQuoteBtn.hidden = true;
+            quoteText.innerText = 'Error Fatal!';
+            console.log('Error Fatal!');
+        }
+    }
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On load
+getQuote();
