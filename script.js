fd61cf21af3afed9df04bc40bf1c1438f458c654
Francisco <ip4c0@icloud.com>
2021-09-23 16:16:24 -0500

    Quote Generator

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function removeLoadingSpinner() {
 //Get Quote from API
 async function getQuote() {
   showLoadingSpinner();
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const proxyUrl = "https://aqueous-garden-74884.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
 
